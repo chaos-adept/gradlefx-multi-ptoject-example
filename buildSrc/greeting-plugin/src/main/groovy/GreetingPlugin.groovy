0dae9e67dc1a91b1260bd3c29ee60d5c22b69c22
chaos-adept <denis.rykovanov@gmail.com>
2013-03-06 16:18:12 +0400

    [+] small refactoring for sparate gradlefx sub module from common buildScr code

--- buildSrc/greeting-plugin/src/main/groovy/GreetingPlugin.groovy
+++ buildSrc/greeting-plugin/src/main/groovy/GreetingPlugin.groovy
@@ -1,10 +1,12 @@
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.apache.commons.lang.StringUtils
 
 class GreetingPlugin implements Plugin<Project> {
     void apply(Project project) {
+		
         project.task('hello') << {
-            println "Hello from the GreetingPlugin ${project.projectDir}"
+            println "Hello new from the GreetingPlugin ${project.projectDir}" + StringUtils.escape(' hello from commons langs')
         }
     }
 }
